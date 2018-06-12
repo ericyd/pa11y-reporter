37199c90afd9a1b4454b0620c8162847207cd56d
Eric <eric@ericyd.com>
2018-06-11 20:47:13 -0700

    fix arrow expand transition

--- templates/scripts.js
+++ templates/scripts.js
@@ -1,20 +1,17 @@
 function toggleNextBlock(e) {
-	var listener = e.currentTarget;
-	// toggle element defined by data-toggle-element
-	document.getElementById(listener.dataset.toggleElement).classList.toggle('expanded');
-	// toggle the arrow icon
-	listener.querySelector('.icon').classList.toggle('is-expanded');
-	// .no-animation supressed animation on load
-	// removing .no-animation on first click ensures it will animate when .is-expanded is removed
-	listener.querySelector('.icon').classList.remove('no-animation');
+    var listener = e.currentTarget;
+    // toggle element defined by data-toggle-element
+    document.getElementById(listener.dataset.toggleElement).classList.toggle('expanded');
+    // toggle the arrow icon
+    listener.querySelector('.icon').classList.toggle('is-expanded');
 }
 
 document.querySelectorAll('.toggler').forEach(function(el) {
-	el.addEventListener('click', toggleNextBlock);
+    el.addEventListener('click', toggleNextBlock);
 })
 
 document.getElementById('expand-all').addEventListener('click', function() {
-	document.querySelectorAll('.toggler').forEach(function(el) {
-		toggleNextBlock({ currentTarget: el });
-	});
+    document.querySelectorAll('.toggler').forEach(function(el) {
+        toggleNextBlock({ currentTarget: el });
+    });
 });
